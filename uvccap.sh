303d5919ee338de2a57c30ee7d7ad6c805716fed
konacam <konacam@debian.(none)>
2010-09-19 17:01:08 -0400

    generalized uvccap

--- uvccap.sh
+++ uvccap.sh
@@ -2,8 +2,9 @@
 while [ 0 = 0 ]
 do
     FILENAME=`date +%Y%m%d%H%M%S01`.jpg
-    uvccapture -x1600 -y1200 -G200 -o${FILENAME}
-    scp ${FILENAME} konacam@mekhi:/var/www/incoming3
+    uvccapture -x1600 -y1200 -G200 -w -o${FILENAME}
+    scp ${FILENAME} $1
     rm ${FILENAME}
     sleep 10
 done
+
